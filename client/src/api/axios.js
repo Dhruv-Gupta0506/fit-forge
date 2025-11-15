bb443cfd38ef1295054f870173c93c2aefbe5a48
Dhruv-Gupta0506 <dhruvgupta0506@gmail.com>
2025-11-15 18:19:31 +0530

    ios issues

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,6 +3,10 @@ import axios from "axios";
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",   
+    "Accept": "application/json",
+  }
 });
 
 export default API;
